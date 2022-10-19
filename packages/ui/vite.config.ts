dede1726c4b32c81aec53555929fc71678b1b96e
Alexandros Dorodoulis <aldor94@gmail.com>
2022-10-19 10:25:23 +0200

    feat: initial implementation

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [svelte({
+      compilerOptions: {
+        customElement: true
+      }
+  })],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'index.ts'),
+      name: 'ui',
+      // the proper extensions will be added
+      fileName: 'ui'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+        }
+      }
+    }
+  }
+})
