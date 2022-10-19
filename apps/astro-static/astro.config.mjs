3cc60f030a0cb109a339f7356a5435e68dc34f4c
Alexandros Dorodoulis <aldor94@gmail.com>
2022-10-19 16:19:05 +0200

    feat: add lit attempts

--- apps/astro-static/astro.config.mjs
+++ apps/astro-static/astro.config.mjs
@@ -1,39 +1,7 @@
 import { defineConfig } from 'astro/config';
-// import customElements from 'custom-elements-ssr/astro.js';
-function getViteConfiguration() {
-  return {
-    optimizeDeps: {
-      include: [
-        'custom-elements-ssr/client-shim.min.js',
-        '@webcomponents/template-shadowroot/template-shadowroot.js'
-      ],
-      exclude: ['custom-elements-ssr/server.js']
-    },
-    ssr: {
-      external: ['linkedom', 'custom-elements-ssr/server.js']
-    }
-  };
-}
+import lit from '@astrojs/lit';
 
-function customElements() {
-  return {
-    name: 'custom-elements-ssr',
-    hooks: {
-      'astro:config:setup': ({ updateConfig, addRenderer }) => {
-        addRenderer({
-          name: 'custom-elements-ssr',
-          clientEntrypoint: 'custom-elements-ssr/client-shim.min.js',
-          serverEntrypoint: 'custom-elements-ssr/server.js'
-        });
-
-        updateConfig({
-          vite: getViteConfiguration()
-        });
-      }
-    }
-  };
-}
 // https://astro.build/config
 export default defineConfig({
-  integrations: [customElements()]
+  integrations: [lit()]
 });
